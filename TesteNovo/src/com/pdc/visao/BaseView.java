75e36f85654b222e5d792d701f612fced77f2113
Thalles <thallesmweb@gmail.com>
2015-11-08 21:29:39 -0200

    commit

--- TesteNovo/src/com/pdc/visao/BaseView.java
+++ TesteNovo/src/com/pdc/visao/BaseView.java
@@ -2,11 +2,13 @@ package com.pdc.visao;
 
 import javax.swing.JFrame;
 
+import com.pdc.ws.dao.Filial;
 import com.pdc.ws.dao.Pessoa;
 
 public class BaseView extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private static Pessoa usuarioLogado;
+	private static Filial filial;
 
 	public static Pessoa getUsuarioLogado() {
 		return usuarioLogado;
@@ -14,5 +16,13 @@ public class BaseView extends JFrame{
 
 	public static void setUsuarioLogado(Pessoa usuarioLogado) {
 		BaseView.usuarioLogado = usuarioLogado;
+	}
+
+	public static Filial getFilial() {
+		return filial;
+	}
+
+	public static void setFilial(Filial filial) {
+		BaseView.filial = filial;
 	}
 }
